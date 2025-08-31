025a3d8b6342113f4e45557d2f5eb83c6a1f566a
James Wolensky <james.wolensky@vtinfo.com>
2025-08-31 07:47:01 -0700

    test: make minimal_init load mini.nvim rtp

--- scripts/minimal_init.lua
+++ scripts/minimal_init.lua
@@ -12,8 +12,26 @@ vim.o.hidden = true
 vim.o.swapfile = false
 vim.o.shada = ''
 
--- Try to make mini.test available if installed
-pcall(vim.cmd, 'packadd mini.test')
+-- Try to make mini.nvim available if installed
+local function maybe_append(path)
+  if path and vim.fn.isdirectory(path) == 1 then
+    if not vim.tbl_contains(vim.opt.runtimepath:get(), path) then
+      vim.opt.runtimepath:append(path)
+    end
+  end
+end
+
+local data = vim.fn.stdpath('data')
+-- Common Lazy path
+maybe_append(data .. '/lazy/mini.nvim')
+-- Common pack/* paths
+maybe_append(data .. '/site/pack/packer/start/mini.nvim')
+maybe_append(data .. '/site/pack/packer/opt/mini.nvim')
+maybe_append(data .. '/site/pack/lazy/start/mini.nvim')
+maybe_append(data .. '/site/pack/vendor/start/mini.nvim')
+maybe_append(data .. '/site/pack/testing/start/mini.nvim')
+
+pcall(vim.cmd, 'packadd mini.nvim')
 
 -- If needed elsewhere: `require("mini.test")` will be done by the runner
 vim.g.__mg_minimal_init = true
